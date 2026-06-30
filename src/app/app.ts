39e4226d35af369b643d29c689d8aad4859e7083
David <cordobaariasd@gmail.com>
2026-06-29 20:17:18 -0600

    feat: agrega informacion al portafolio

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,17 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { About } from './components/about/about';
+import { Contact } from './components/contact/contact';
+import { Experience } from './components/experience/experience';
+import { Footer } from './components/footer/footer';
+import { Hero } from './components/hero/hero';
+import { Navbar } from './components/navbar/navbar';
+import { Projects } from './components/projects/projects';
+import { Skills } from './components/skills/skills';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Navbar, Hero, About, Skills, Projects, Experience, Contact, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
